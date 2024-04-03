5f7065dfa715e68ff2f47907fa00ab0add110d86
Elga <elga.lika@fti.edu.al>
2024-04-03 15:43:18 +0200

    1 Commit

---
+++ Lab1/springlab1/src/main/java/com/springlab1/springlab1/Springlab1Application.java
@@ -0,0 +1,15 @@
+package com.springlab1.springlab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springlab1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springlab1Application.class, args);
+
+		System.out.println("hello world");
+	}
+
+}
---
+++ Lab1/springlab1/src/test/java/com/springlab1/springlab1/Springlab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springlab1.springlab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springlab1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
